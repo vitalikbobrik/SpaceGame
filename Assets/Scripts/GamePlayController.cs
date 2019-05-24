731f9d224734d9ae7a695f745bd1d71ddf469f18
vitalikbobrik <vitalikbobrok@gmail.com>
2019-05-24 11:35:02 +0300

    first commit / models / simple player control

---
+++ Assets/Scripts/GamePlayController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GamePlayController : MonoBehaviour
+{
+    [SerializeField] private GameObject m_levelArt;
+    [SerializeField] private GameObject m_deathPanel;
+    [SerializeField] private float m_speed = 5;
+    [SerializeField] private TextMeshProUGUI m_scoreText;
+    [SerializeField] private TextMeshProUGUI m_highScoreText;
+    [SerializeField] private TextMeshProUGUI m_endLevelScoreText;
+    private float m_score = 0;
+    private float m_deathTimer = 0;
+    private bool isRotating = false;
+    private bool isGameOver = false;
+    private Animator anim;
+    
+
+    private void Start()
+    {
+        m_deathPanel.SetActive(false);
+        isGameOver = false;
+        anim = GetComponent<Animator>();
+
+    }
+
+    void Update()
+    {
+        if (!isGameOver)
+        {
+            CheckInput();
+        }
+        CheckObstacles();
+        UpdateScore();
+    }
+
+    private void UpdateScore()
+    {
+        if (!isGameOver)
+        {
+            m_score += Time.deltaTime * 10;
+            m_scoreText.text = "Score: " + (int)m_score;
+        }
+    }
+
+    private void CheckObstacles()
+    {
+        RaycastHit hit;
+        //check walls
+        if (!Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 0.5f))
+        {
+            //move player forward(speed increases at every 100 scorepoints)
+            transform.Translate(Vector3.forward * (m_speed+m_score/100) * Time.deltaTime);
+            anim.SetInteger("AnimationPar", 1);
+            m_deathTimer = 0; 
+        }
+        else
+        {
+            //if player is behind the wall more than 0.2sec , lose screeen will appear
+            m_deathTimer += Time.deltaTime;
+            anim.SetInteger("AnimationPar", 0); 
+            if (m_deathTimer > 0.2f)
+            {
+                isGameOver = true;
+                m_deathPanel.SetActive(true);
+                m_endLevelScoreText.text = "Your score: " + (int)m_score;
+                if (PlayerPrefs.GetInt("Score") < (int)m_score)
+                {
+                    PlayerPrefs.SetInt("Score", (int)m_score);
+                }
+                m_highScoreText.text = "Highscore: " + PlayerPrefs.GetInt("Score");
+            }
+        }
+    }
+
+    private void CheckInput()
+    {
+        if (Input.GetMouseButton(0) && !isRotating)
+        {
+            StartCoroutine(Rotate(Vector3.right, -90, 0.3f));
+        }
+        if (Input.GetMouseButton(1) && !isRotating)
+        {
+            StartCoroutine(Rotate(Vector3.right, 90, 0.3f));
+        }
+    }
+
+    IEnumerator Rotate(Vector3 axis, float angle, float duration = 1.0f)
+    {
+        //rotating walls
+        isRotating = true;
+        Quaternion from = m_levelArt.transform.rotation;
+        Quaternion to = m_levelArt.transform.rotation;
+        to *= Quaternion.Euler(axis * angle);
+
+        float elapsed = 0.0f;
+        while (elapsed < duration)
+        {
+            m_levelArt.transform.rotation = Quaternion.Slerp(from, to, elapsed / duration);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+        m_levelArt.transform.rotation = to;
+        isRotating = false;
+    }
+}
---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public enum CubeSide
+    {
+        Left,
+        Right,
+        Up,
+        Down
+    }
+    [SerializeField] private GameObject m_sideCube;
+    [SerializeField] private GameObject m_cubePrefab;
+    //[SerializeField] private GameObject m_coinPrefab;
+    private Queue<GameObject> availableObjects = new Queue<GameObject>();
+    public static LevelGenerator Instance { get; private set; }
+    private GameObject m_cube;
+    private GameObject m_levelPart;
+    private int currentCubeSide = -1;
+
+    private void Awake()
+    {
+        Instance = this;
+        
+       // GrowPool();
+    }
+
+    private void Start()
+    {
+        BuildLevel();
+    }
+
+    private void BuildLevel()
+    {
+        for (int i = 0; i < 2000; i++)
+        {
+            m_cube = Instantiate(m_cubePrefab, new Vector3(i, 0, 0),Quaternion.Euler(new Vector3(90,0,90)));
+            m_cube.transform.SetParent(transform);
+            if (i % 2 == 0 && i>10)
+            {
+                #region BuildSideCube
+                    var sideCube = Instantiate(m_sideCube, transform.position, Quaternion.identity);
+                    var typeSideCube = (CubeSide)UnityEngine.Random.Range(0, Enum.GetValues(typeof(CubeSide)).Length);
+                    sideCube.transform.SetParent(m_cube.transform);
+                    switch (typeSideCube)
+                    {
+                        case CubeSide.Left:
+                            sideCube.transform.position = new Vector3(i, 0, 0.5f);
+                            sideCube.transform.rotation = Quaternion.Euler(0, -90, 0);
+                            break;
+                        case CubeSide.Right:
+                            sideCube.transform.position = new Vector3(i, 0, -0.5f);
+                            sideCube.transform.rotation = Quaternion.Euler(0, 90, 0);
+
+                            break;
+                        case CubeSide.Up:
+                            sideCube.transform.position = new Vector3(i, 0.5f, 0);
+                            sideCube.transform.rotation = Quaternion.Euler(0, 0, 90);
+
+                            break;
+                        case CubeSide.Down:
+                            sideCube.transform.position = new Vector3(i, -0.5f, 0);
+                            sideCube.transform.rotation = Quaternion.Euler(0, 0, -90);
+                            break;
+
+                    }
+                    sideCube.transform.localScale = new Vector3(UnityEngine.Random.Range(1, 10), 1, 1);
+                #endregion
+            }
+        }
+    }
+
+
+    //private void GrowPool()
+    //{
+    //    for (int i = 0; i < 10; i++)
+    //    {
+    //        var instanceToAdd = Instantiate(m_levelPart);
+    //        instanceToAdd.transform.SetParent(transform);
+    //        AddToPool(instanceToAdd);
+    //    }
+    //}
+
+    //private void AddToPool(GameObject instance)
+    //{
+    //    instance.SetActive(false);
+    //    availableObjects.Enqueue(instance);
+    //}
+
+    //public GameObject GetFromPool()
+    //{
+    //    if (availableObjects.Count == 0)
+    //        GrowPool();
+    //    var instance = availableObjects.Dequeue();
+    //    instance.SetActive(true);
+    //    return instance;
+    //}
+}
